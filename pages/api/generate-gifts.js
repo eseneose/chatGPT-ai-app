354851799012452d10e31245db144d18304ffd5a
unknown <eseneose96@gmail.com>
2023-01-25 00:45:12 -0800

    initial commit

--- pages/api/generate-gifts.js
+++ pages/api/generate-gifts.js
@@ -1,4 +1,5 @@
 import { Configuration, OpenAIApi } from "openai";
+import  "regenerator-runtime";
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -6,15 +7,13 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
-  const { priceMin, priceMax, gender, age, hobbies } = req.body;
+  const { prompt } = req.body;
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: generatePrompt(priceMin, priceMax, gender, age, hobbies),
+    prompt: prompt,
     temperature: 0.6,
     max_tokens: 2048,
   });
   res.status(200).json({ result: completion.data.choices[0].text });
 }
-function generatePrompt(priceMin, priceMax, gender, age, hobbies) {
-  return `suggest 3 Christmas gift ideas between ${priceMin}$ and ${priceMax}$ for a ${age} years old ${gender} that is into ${hobbies}.`;
-}
+
